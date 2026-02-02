d2a188d15b018d7a1fcba57100c6512b70764996
EltayebLobah <eltayebalaa@gmail.com>
2026-02-02 19:24:51 +0200

    Merge LifetimeScopes

---
+++ Packages/ITI.DesignPatterns.CustomPackage/Runtime/GameLifeTimeScope.cs
@@ -0,0 +1,20 @@
+using ITI.DesignPatterns.Foundation.Runtime.Event;
+using VContainer;
+using VContainer.Unity;
+
+namespace ITI.DesignPatterns.CustomPackage.Runtime
+{
+    public class GameLifeTimeScope : LifetimeScope
+    {
+        protected override void Configure(IContainerBuilder builder)
+        {
+            builder.Register<IEventSystem, EventSystem>(Lifetime.Singleton);
+
+            //builder.Register<IGameManager, GameManager>(Lifetime.Singleton);
+            //builder.Register<IEnemiesManager, EnemyManager>(Lifetime.Singleton);
+            //builder.Register<IUIManager, UIManager>(Lifetime.Singleton);
+            //builder.Register<IWaveManager, WaveManager>(Lifetime.Singleton);
+            //builder.Register<ITowerManager, TowerManager>(Lifetime.Singleton);
+        }
+    }
+}
